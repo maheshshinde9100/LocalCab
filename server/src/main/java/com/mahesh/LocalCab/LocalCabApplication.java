b3e380209f36a2178845be9711449b4ff5a3ea9e
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-02-16 21:40:57 +0530

    springboot backend intial commit

---
+++ server/src/main/java/com/mahesh/LocalCab/LocalCabApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.LocalCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCabApplication.class, args);
+	}
+
+}
