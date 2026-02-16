0df8e81a0ada05ad228a7350a9853aff5f4b062e
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-02-16 22:01:46 +0530

    .

--- server/src/main/java/com/mahesh/LocalCab/LocalCabApplication.java
+++ server/src/main/java/com/mahesh/LocalCab/LocalCabApplication.java
@@ -2,8 +2,10 @@ package com.mahesh.LocalCab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LocalCabApplication {
 
 	public static void main(String[] args) {
